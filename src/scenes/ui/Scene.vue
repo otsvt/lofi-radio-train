6028c789d85fd7ef6d0313ee36c2355692674f97
AlexandrSamoylenko <heylalalay@mail.ru>
2026-04-24 18:30:33 +0400

    feat: add and use scene-track

--- src/scenes/ui/Scene.vue
+++ src/scenes/ui/Scene.vue
@@ -3,7 +3,12 @@
     :class="['scene translate-z-2 transition-all', zIndex, isZooming ? 'blur-xs scale-101' : '']"
     :style="{ transform: `rotateY(${moveY}deg) rotateX(${moveX}deg)` }"
   >
-    <div class="layer bg-center" :style="{ backgroundImage: `url(${scene1})` }"></div>
+    <div class="scene-track">
+      <div class="scene-layer animation-move">
+        <div class="scene-frame" :style="{ backgroundImage: `url(${scene1})` }"></div>
+        <div class="scene-frame" :style="{ backgroundImage: `url(${scene1})` }"></div>
+      </div>
+    </div>
   </article>
 </template>
 
@@ -17,3 +22,19 @@
 
   const { moveX, moveY, isZooming } = useCameraMovement();
 </script>
+
+<style scoped>
+  @keyframes move {
+    from {
+      transform: translateX(0);
+    }
+
+    to {
+      transform: translateX(-50%);
+    }
+  }
+
+  .animation-move {
+    animation: move 60s linear infinite;
+  }
+</style>
