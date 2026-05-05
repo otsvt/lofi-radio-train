dae69aeaadfd2788e1ef72f348481cd6906f480c
AlexandrSamoylenko <heylalalay@mail.ru>
2026-05-05 14:48:46 +0400

    style: update start-button variant

--- src/shared/components/buttons/start-button/ui/StartButton.vue
+++ src/shared/components/buttons/start-button/ui/StartButton.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <button
     class="relative p-2 flex-center border-2 rounded transition-all border-slate-300/50 backdrop-blur-xs -rotate-y-6 rotate-x-70 -rotate-z-10 hover:rotate-x-0 hover:rotate-y-0 hover:rotate-z-0"
   >
@@ -17,8 +17,33 @@
       <path d="M21 16v2a4 4 0 0 1-4 4h-5" />
     </svg>
   </button>
+</template> -->
+
+<template>
+  <button
+    class="relative p-2 flex-center border-2 rounded transition-all border-slate-300/50 backdrop-blur-xs -rotate-y-6 rotate-x-70 -rotate-z-10 hover:scale-110 hover:ring-8 ring-slate-100/20"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="h-10 w-10 stroke-slate-300/50"
+    >
+      <path d="M12 7v4" />
+      <path d="M7.998 9.003a5 5 0 1 0 8-.005" />
+      <rect x="3" y="3" width="18" height="18" rx="2" />
+    </svg>
+  </button>
 </template>
 
 <script setup lang="ts"></script>
+
+<!-- <path
+        d="M3 11h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-5Zm0 0a9 9 0 1 1 18 0m0 0v5a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3Z"
+      />
+      <path d="M21 16v2a4 4 0 0 1-4 4h-5" /> -->
 
 <!-- <path d="M12 7v4"/><path d="M7.998 9.003a5 5 0 1 0 8-.005"/><rect x="3" y="3" width="18" height="18" rx="2"/> -->
