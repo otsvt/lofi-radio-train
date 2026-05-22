b4468024d6e5182e5360c8d7d9d185c6c64354d4
AlexandrSamoylenko <heylalalay@mail.ru>
2026-05-22 17:31:12 +0400

    feat: create action button

--- src/features/scenes/ui/components/SceneSetting.vue
+++ src/features/scenes/ui/components/SceneSetting.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="space-y-4">
-    <p class="text-3xl uppercase">scene</p>
+  <div class="space-y-4 uppercase">
+    <p class="text-3xl">scene</p>
     <div class="flex items-center gap-x-4 text-2xl">
       <SettingButton symbols="<" />
-      <span class="uppercase">night city</span>
+      <span>{{ scene.label }}</span>
       <SettingButton symbols=">" />
     </div>
+    <ActionButton class="mx-auto" label="[ apply ]" :disabled="true" />
   </div>
 </template>
 
 <script setup lang="ts">
+  import { ref } from "vue";
+  import { ActionButton } from "@/shared/components/buttons/action-button";
   import { SettingButton } from "@/shared/components/buttons/setting-button";
+
+  const scenes = {
+    nightCity: {
+      label: "night city",
+    },
+  };
+
+  const scene = ref(scenes.nightCity);
 </script>
---
+++ src/shared/components/buttons/action-button/ui/ActionButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <button
+    class="px-2 flex-center text-2xl font-bold uppercase transition-opacity disabled:opacity-30"
+    :disabled="disabled"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script setup lang="ts">
+  defineProps<{ disabled?: boolean; label: string }>();
+</script>
